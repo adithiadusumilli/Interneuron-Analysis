02e8e5a61710601d8a603d1f93c270ac28c2ff12
adithiadusumilli <147884503+adithiadusumilli@users.noreply.github.com>
2026-03-17 21:49:57 -0500

    Update plotSavedChunkXCByBehavior_perAnimal.m

--- plotSavedChunkXCByBehavior_perAnimal.m
+++ plotSavedChunkXCByBehavior_perAnimal.m
@@ -10,7 +10,7 @@ function plotSavedChunkXCByBehavior_perAnimal(chunkFile, minTrials)
 
 arguments
     chunkFile (1,1) string
-    minTrials (1,1) double = 50
+    minTrials (1,1) double = 40
 end
 
 behNames = {'unlabeled','climbdown','climbup','eating','grooming','jumpdown','jumping','rearing','still','walkflat','walkgrid'};
